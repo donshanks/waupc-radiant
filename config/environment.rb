10af0613df5fda848fdadc0d77948c2be7d22771
Don Shanks <donshanks@gmail.com>
2010-06-29 05:03:39 +0000

    checkpoint

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 require 'radius'
 
 Radiant::Initializer.run do |config|
-  config.frameworks -= [ :action_mailer ]
+#  config.frameworks -= [ :action_mailer ]
 
   config.action_controller.session = {
     :session_key => '_waupc-radiant_session',
@@ -30,9 +30,9 @@ Radiant::Initializer.run do |config|
     ActiveSupport::Inflector.inflections do |inflect|
       inflect.uncountable 'config'
     end
-#    Radiant::Config['admin.title'] = "Washington District UPCI"
-#    Radiant::Config['admin.subtitle'] = "Bringing the Gospel to Washington State"
-#    Radiant::Config['defaults.page.parts'] = "body,sidebar"
-#    Radiant::Config['defaults.page.filter'] = "Fckeditor"
+    Radiant::Config['admin.title'] = "Washington District UPCI"
+    Radiant::Config['admin.subtitle'] = "Bringing the Gospel to Washington State"
+    Radiant::Config['defaults.page.parts'] = "body,sidebar"
+    Radiant::Config['defaults.page.filter'] = "Fckeditor"
   end
 end
